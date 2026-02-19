eff2185fb88e49f0e8662c6ba8362a6e676c148c
Dhiauddin <dhiauddin@gmail.com>
2026-02-19 17:30:08 +0800

    -- update app.blade

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { path  } from 'path';
+// import { path  } from 'path';
 
 export default defineConfig({
 	plugins: [
@@ -13,7 +13,51 @@ export default defineConfig({
 			refresh: true,
 		}),
 	],
+	// define: {
+	// 	jQuery: 'window.jQuery',
+	// 	$: 'window.jQuery'
+	// },
 	build: {
-		chunkSizeWarningLimit: 4000,
+		chunkSizeWarningLimit: 5000,
+
+		// only for javascript so.... no CSS
+		// and also, not practical due to track all package.json
+		// rollupOptions: {
+		// 	output: {
+		// 		manualChunks: {
+		// 			vendorjs: [
+		// 				'@claviska/jquery-minicolors',
+		// 				'@fullcalendar/core',
+		// 				'@fullcalendar/daygrid',
+		// 				'@fullcalendar/list',
+		// 				'@fullcalendar/moment',
+		// 				'@fullcalendar/multimonth',
+		// 				'@fullcalendar/timegrid',
+		// 				'addremrow-validator5-swal2-ajax',
+		// 				'bootstrapvalidator5',
+		// 				'chart.js',
+		// 				'ckeditor5',
+		// 				'datatables.net',
+		// 				'datatables.net-autofill-bs5',
+		// 				'datatables.net-bs5',
+		// 				'datatables.net-buttons-bs5',
+		// 				'datatables.net-responsive-bs5',
+		// 				'jquery',
+		// 				'jquery-ui',
+		// 				'jszip',
+		// 				'moment',
+		// 				'pdfmake',
+		// 				'select2',
+		// 				'select2-bootstrap-5-theme',
+		// 				'sweetalert2',
+		// 			]
+		// 		}
+		// 	}
+		// },
+
+		sourcemap: true,        // full source map
+		// sourcemap: 'inline', // embed in JS file
+		// sourcemap: 'hidden', // generate but don't expose in devtools
 	},
 });
+
